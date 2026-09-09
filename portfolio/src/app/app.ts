0985737c516fa4b5646812995254aa7826b00913
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-09-09 14:39:40 +0530

    [Rudresh] : update

---
+++ portfolio/src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Experience } from './components/experience/experience';
+import { Contact } from './components/contact/contact';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Navbar,Hero,About,Skills,Projects,Experience,Contact],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('portfolio');
+}
---
+++ portfolio/src/app/components/navbar/navbar.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.html'
+})
+export class Navbar {
+
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+}
